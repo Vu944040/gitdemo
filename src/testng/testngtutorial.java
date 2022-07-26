27a2d8c3b485d66cc46758f3a95e016d81ba918a
vaseem <vaseemudiin1997@gmail.com>
2022-07-26 15:50:34 +0530

    first commit

---
+++ src/testng/testngtutorial.java
@@ -0,0 +1,18 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class testngtutorial {
+
+	@Test
+	public void demo()
+	{
+		System.out.println("vaseem");
+	}
+	
+	@Test
+	public void intro()
+	{
+		System.out.println("imran");
+	}
+}
